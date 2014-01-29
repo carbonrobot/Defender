2052f8e7e430f89f6326004491ed49299e4724f3
Charlie Brown <carbonrobot@gmail.com>
2014-01-29 07:41:08 -0800

    Revert to claims for testing

--- Shield/PresharedKeyAuthorizer.cs
+++ Shield/PresharedKeyAuthorizer.cs
@@ -17,20 +17,15 @@ namespace Shield
     {
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
         {
-            //var claims = new List<Claim>();
-            //claims.Add(new Claim(ClaimTypes.Name, "superstar"));
-            
-            //var identity = new ClaimsIdentity(claims, "PresharedKey");
-            //var principal = new ClaimsPrincipal(identity);
-            
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, "superstar"));
+
+            var identity = new ClaimsIdentity(claims, "PresharedKey");
+            var principal = new ClaimsPrincipal(identity);
+
             //Thread.CurrentPrincipal = principal;
             //if (HttpContext.Current != null)
             //    HttpContext.Current.User = principal;
-
-            var principal = new System.Security.Principal.GenericPrincipal(new System.Security.Principal.GenericIdentity("superstar", "PresharedKey"), null);
-            Thread.CurrentPrincipal = principal;
-            if (HttpContext.Current != null)
-                HttpContext.Current.User = principal;
 
             return base.SendAsync(request, cancellationToken);
         }
